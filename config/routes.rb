d02f2d3f78a6f0672b9dae4ea134f65565e56dec
Dark-Star3110 <dangvan31102001@gmail.com>
2023-02-06 12:35:52 +0700

    edit build

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'user/show'
+
+  get 'home/index'
+
+  devise_for :admins
+  get 'pages/how_to_write'
+
+  get 'pages/Myblogs'
+
+  devise_for :users
+  resources :blogs
+  resources :users
+  
+  get 'pages/signup'
+  root 'home#index'
+  
+  get 'pages/home'
+
+  get 'pages/contactus'
+
+  get 'pages/about'
+  
+  get 'pages/show_detailed_blog'
+  get 'user/index'
+  
+  
+
+
+  
+ 
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
