3d9b3799306315e80e051f1c13483299ac590f7e
Phuc <phucbopbnation@gmail.com>
2023-02-10 21:42:58 +0700

    view in smartphone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
-  def index
+  #view in smartphone
+  layout :select_layout
+
+  def select_layout
+    session.inspect # force session load
+    if session.has_key? "layout"
+      return (session["layout"] == "mobile") ? "mobile_application" : "application"
+    end
+    return detect_browser
+  end
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return "mobile_application" if agent.match(m)
+    end
+    return "application"
   end
 end
