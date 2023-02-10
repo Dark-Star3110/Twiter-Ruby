9795479b60595403c7139586bb19eb1bae9ff90e
Phuc <phucbopbnation@gmail.com>
2023-02-10 21:56:55 +0700

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,4 @@
 class HomeController < ApplicationController
-  #view in smartphone
-  layout :select_layout
-
-  def select_layout
-    session.inspect # force session load
-    if session.has_key? "layout"
-      return (session["layout"] == "mobile") ? "mobile_application" : "application"
-    end
-    return detect_browser
-  end
-
-  def detect_browser
-    agent = request.headers["HTTP_USER_AGENT"].downcase
-    MOBILE_BROWSERS.each do |m|
-      return "mobile_application" if agent.match(m)
-    end
-    return "application"
+  def index
   end
 end
